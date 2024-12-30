fe2d95e97f04cc215ed972085c0e9af5e599881e
Aaryan Singh <aaryan.jsingh@gmail.com>
2024-12-29 18:52:06 -0700

    multer setup

--- routes/users-routes.js
+++ routes/users-routes.js
@@ -3,11 +3,13 @@ const router = express.Router();
 const { check } = require("express-validator");
 
 const userController = require("../controllers/users-controllers");
+const fileUpload = require("../middleware/file-upload");
 
 router.get("/", userController.getAllUsers);
 
 router.post(
   "/signup",
+  fileUpload.single('image'),
   [
     check("name").not().isEmpty(),
     check("email").isEmail(),
